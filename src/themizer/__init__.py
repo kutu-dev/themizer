e392695352fbcc022a162e9a501aa2b3a9e98fa2
Kutu <the.kutu.dev@gmail.com>
2022-12-23 00:24:33 +0100

    Fix typo in pyproject.toml

--- src/themizer/__init__.py
+++ src/themizer/__init__.py
@@ -1,7 +1,7 @@
 # themizer - Created with ♥ by Kutu (https://kutu-dev.github.io/)
 
 # Define global variables
-__version__: str = '1.0.0'
+__version__: str = '1.0.1'
 APP_NAME: str = 'themizer'
 
 # Change the controller name to a more pleasant one
